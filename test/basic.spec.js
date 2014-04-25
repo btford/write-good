a2d8cd24941bbe7a0d857abd0776c7f05f82b5fb
Brian Ford <btford@umich.edu>
2014-04-25 00:48:08 -0700

    feat: detect lexical illusions

--- test/basic.spec.js
+++ test/basic.spec.js
@@ -14,6 +14,24 @@ describe('writeGood', function () {
     ]);
   });
 
+  it('should detect lexical illusions', function () {
+    expect(writeGood('the the')).toEqual([
+      { index: 4, offset: 3, reason: '"the" is repeated' }
+    ]);
+  });
+
+  it('should detect lexical illusions with line breaks', function () {
+    expect(writeGood('the\nthe')).toEqual([
+      { index: 4, offset: 3, reason: '"the" is repeated' }
+    ]);
+  });
+
+  it('should detect lexical illusions with case insensitivity', function () {
+    expect(writeGood('The the')).toEqual([
+      { index: 4, offset: 3, reason: '"the" is repeated' }
+    ]);
+  });
+
   it('should order suggestions by index', function () {
     expect(writeGood('It has been said that few developers write well.')).toEqual([
       { index: 7, offset: 9, reason: '"been said" is passive voice' },
--- write-good.js
+++ write-good.js
@@ -1,9 +1,11 @@
-var weasels = require('weasel-words'),
-    passive = require('passive-voice');
+var weasels  = require('weasel-words'),
+    illusion = require('./lib/lexical-illusions'),
+    passive  = require('passive-voice');
 
 module.exports = function (text, opts) {
   return weasels(text).map(reasonable('is a weasel word')).
       concat(passive(text).map(reasonable('is passive voice'))).
+      concat(illusion(text).map(reasonable('is repeated'))).
       sort(function (a, b) {
         return a.index < b.index ? -1 : 1;
       });
