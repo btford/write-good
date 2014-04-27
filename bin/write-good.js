40df44e79c9d11605569e32ad0987475ab69cffd
Brian Ford <btford@umich.edu>
2014-04-27 06:24:01 -0700

    feat: add flags to CLI for different checks

--- bin/write-good.js
+++ bin/write-good.js
@@ -4,7 +4,39 @@ var glob      = require('glob');
 var fs        = require('fs');
 var writeGood = require('../write-good');
 var annotate  = require('../lib/annotate');
-var patterns  = process.argv.slice(2);
+
+var args      = process.argv.slice(2);
+var patterns  = args.filter(function (arg) {
+  return arg.substr(0, 2) !== '--';
+});
+
+var opts      = {
+  illusion : null,
+  passive  : null,
+  so       : null,
+  weasel   : null
+};
+
+var include = true;
+
+args.filter(function (arg) {
+  return arg.substr(0, 2) === '--';
+}).map(function (arg) {
+  return arg.substr(2);
+}).forEach(function (arg) {
+  if (arg.substr(0, 3) === 'no-') {
+    opts[arg.substr(3)] = false;
+  } else {
+    opts[arg] = true;
+    include = false;
+  }
+});
+
+Object.keys(opts).forEach(function (name) {
+  if (typeof opts[name] !== 'boolean') {
+    opts[name] = include;
+  }
+});
 
 // default
 if (patterns.length === 0) {
@@ -18,7 +50,7 @@ patterns.forEach(function (pattern) {
     } else {
       files.forEach(function (file) {
         var contents = fs.readFileSync(file, 'utf8');
-        var suggestions = writeGood(contents);
+        var suggestions = writeGood(contents, opts);
 
         if (suggestions.length) {
           console.log('In ' + file);
