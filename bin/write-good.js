fdba9df13fbfc4c85d99fba377f44abdee4ad9e1
Brian Ford <btford@umich.edu>
2014-05-19 16:09:26 -0700

    fix: remove dependency on glob
    Closes #6

--- bin/write-good.js
+++ bin/write-good.js
@@ -1,14 +1,18 @@
 #!/usr/bin/env node
 
-var glob      = require('glob');
 var fs        = require('fs');
 var writeGood = require('../write-good');
 var annotate  = require('../lib/annotate');
 
 var args      = process.argv.slice(2);
-var patterns  = args.filter(function (arg) {
+var files     = args.filter(function (arg) {
   return arg.substr(0, 2) !== '--';
 });
+
+if (files.length === 0) {
+  console.log('You did not provide any files to check');
+  process.exit(1);
+}
 
 var opts      = {
   illusion : null,
@@ -38,27 +42,14 @@ Object.keys(opts).forEach(function (name) {
   }
 });
 
-// default
-if (patterns.length === 0) {
-  patterns.push('**/*.md');
-}
+files.forEach(function (file) {
+  var contents = fs.readFileSync(file, 'utf8');
+  var suggestions = writeGood(contents, opts);
 
-patterns.forEach(function (pattern) {
-  glob(pattern, function (err, files) {
-    if (err) {
-      return console.log(err);
-    } else {
-      files.forEach(function (file) {
-        var contents = fs.readFileSync(file, 'utf8');
-        var suggestions = writeGood(contents, opts);
-
-        if (suggestions.length) {
-          console.log('In ' + file);
-          console.log('=============');
-          console.log(annotate(contents, suggestions).join('\n-------------\n'));
-        }
-      });
-    }
-  });
+  if (suggestions.length) {
+    console.log('In ' + file);
+    console.log('=============');
+    console.log(annotate(contents, suggestions).join('\n-------------\n'));
+  }
 });
 
