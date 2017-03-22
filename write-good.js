643d4c3b8848c22f8a260bae2878a81288c8594f
Paul Miller <paul@dgtlrepublic.com>
2017-03-22 03:53:09 -0400

    Object.Assign Poly

--- write-good.js
+++ write-good.js
@@ -55,4 +55,29 @@ function dedup (suggestions) {
   }, []);
 }
 
+// Mozilla Object.Assign Polyfill
+if (typeof Object.assign != 'function') {
+  Object.assign = function(target, varArgs) {
+    'use strict';
+    if (target == null) {
+      throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    var to = Object(target);
+
+    for (var index = 1; index < arguments.length; index++) {
+      var nextSource = arguments[index];
+
+      if (nextSource != null) {
+        for (var nextKey in nextSource) {
+          if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+            to[nextKey] = nextSource[nextKey];
+          }
+        }
+      }
+    }
+    return to;
+  };
+}
+
 module.exports.annotate = require('./lib/annotate');
