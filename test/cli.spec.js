8ff93f67a1d73ac35c553f7673e8a9aa5de3c25e
Matt Blair <matt@mattblair.co>
2019-01-16 17:45:52 -0800

    add eslint; auto fixes

--- test/cli.spec.js
+++ test/cli.spec.js
@@ -1,8 +1,7 @@
-var exec = require('child_process').exec;
+const exec = require('child_process').exec;
 
-describe('CLI', function() {
-
-  var expectedWarningsNotWeasel = [
+describe('CLI', () => {
+  const expectedWarningsNotWeasel = [
     '"was killed" may be passive voice on line 3 at column 11',
     '"the" is repeated on line 5 at column 4',
     '"There is" is unnecessary verbiage on line 9 at column 0',
@@ -15,30 +14,30 @@ describe('CLI', function() {
   ];
 
   // warnings for phrases that cause only 'weasel' warnings:
-  var expectedWarningsOnlyWeasel = [
+  const expectedWarningsOnlyWeasel = [
     '"few" is a weasel word on line 1 at column 11',
     '"few" is a weasel word on line 17 at column 22'
   ];
 
   // 'weasel' warnings for phrases that cause 'weasel' warnings and other warnings:
-  var expectedWarningsPartWeaselOnly = [
+  const expectedWarningsPartWeaselOnly = [
     '"Remarkably" is a weasel word on line 1 at column 0',
     '"extremely" is a weasel word on line 15 at column 17'
   ];
 
   // combined warnings for phrases that cause 'weasel' warnings and other warnings:
-  var expectedWarningsPartWeaselPlus = [
+  const expectedWarningsPartWeaselPlus = [
     '"Remarkably" is a weasel word and can weaken meaning on line 1 at column 0',
     '"extremely" is a weasel word and can weaken meaning on line 15 at column 17'
   ];
 
   // other warnings for phrases that cause 'weasel' warnings and other warnings:
-  var expectedWarningsPartWeaselMinus = [
+  const expectedWarningsPartWeaselMinus = [
     '"Remarkably" can weaken meaning on line 1 at column 0',
     '"extremely" can weaken meaning on line 15 at column 17'
   ];
 
-  var expectedWarningsEprime = [
+  const expectedWarningsEprime = [
     '"was" is a form of \'to be\' on line 3 at column 11',
     '"is" is a form of \'to be\' on line 9 at column 6',
     '"are" is a form of \'to be\' on line 11 at column 6',
@@ -50,120 +49,120 @@ describe('CLI', function() {
     '"is" is a form of \'to be\' on line 25 at column 8'
   ];
 
-  it('should provide the basic functionality as expected (smoke test)', function(done) {
-    exec('./bin/write-good.js ./test/texts/English.md', function(err, stdout, stderr) {
-        var expectedWarnings = expectedWarningsNotWeasel
-          .concat(expectedWarningsOnlyWeasel)
-          .concat(expectedWarningsPartWeaselPlus);
-        expectedWarnings.forEach(function(warning) {
-          expect(stdout.includes(warning)).toBe(true);
-        });
-        done();
-    });
-  });
-
-  it('should support running only the checks provided as command line arguments', function(done) {
-    exec('./bin/write-good.js test/texts/English.md --weasel', function(err, stdout, stderr) {
-      expectedWarningsNotWeasel.forEach(function(warning) {
-        expect(stdout.includes(warning)).toBe(false);
-      });
-      var expectedWarnings = expectedWarningsOnlyWeasel.concat(expectedWarningsPartWeaselOnly);
-      expectedWarnings.forEach(function(warning) {
+  it('should provide the basic functionality as expected (smoke test)', (done) => {
+    exec('./bin/write-good.js ./test/texts/English.md', (err, stdout, stderr) => {
+      const expectedWarnings = expectedWarningsNotWeasel
+        .concat(expectedWarningsOnlyWeasel)
+        .concat(expectedWarningsPartWeaselPlus);
+      expectedWarnings.forEach((warning) => {
         expect(stdout.includes(warning)).toBe(true);
       });
       done();
     });
   });
 
-  it('should support deactivating checks', function(done) {
-    exec('./bin/write-good.js test/texts/English.md --no-weasel', function(err, stdout, stderr) {
-      var notExpectedWarnings = expectedWarningsOnlyWeasel.concat(expectedWarningsPartWeaselOnly);
-      notExpectedWarnings.forEach(function(warning) {
+  it('should support running only the checks provided as command line arguments', (done) => {
+    exec('./bin/write-good.js test/texts/English.md --weasel', (err, stdout, stderr) => {
+      expectedWarningsNotWeasel.forEach((warning) => {
         expect(stdout.includes(warning)).toBe(false);
       });
-      var expectedWarnings = expectedWarningsNotWeasel.concat(expectedWarningsPartWeaselMinus);
-      expectedWarnings.forEach(function(warning) {
+      const expectedWarnings = expectedWarningsOnlyWeasel.concat(expectedWarningsPartWeaselOnly);
+      expectedWarnings.forEach((warning) => {
         expect(stdout.includes(warning)).toBe(true);
       });
       done();
     });
   });
 
-  it('should not check for E-Prime compliance if not explicitly activated', function(done) {
-    exec('./bin/write-good.js ./test/texts/English.md', function(err, stdout, stderr) {
-      expectedWarningsEprime.forEach(function(warning) {
+  it('should support deactivating checks', (done) => {
+    exec('./bin/write-good.js test/texts/English.md --no-weasel', (err, stdout, stderr) => {
+      const notExpectedWarnings = expectedWarningsOnlyWeasel.concat(expectedWarningsPartWeaselOnly);
+      notExpectedWarnings.forEach((warning) => {
         expect(stdout.includes(warning)).toBe(false);
       });
-      done();
-    });
-  });
-
-  it('should check for E-Prime compliance if explicitly activated', function(done) {
-    exec('./bin/write-good.js ./test/texts/English.md --eprime', function(err, stdout, stderr) {
-      expectedWarningsEprime.forEach(function(warning) {
+      const expectedWarnings = expectedWarningsNotWeasel.concat(expectedWarningsPartWeaselMinus);
+      expectedWarnings.forEach((warning) => {
         expect(stdout.includes(warning)).toBe(true);
       });
       done();
     });
   });
 
-  it('should support extensions', function(done) {
-    var expectedWarnings = [
+  it('should not check for E-Prime compliance if not explicitly activated', (done) => {
+    exec('./bin/write-good.js ./test/texts/English.md', (err, stdout, stderr) => {
+      expectedWarningsEprime.forEach((warning) => {
+        expect(stdout.includes(warning)).toBe(false);
+      });
+      done();
+    });
+  });
+
+  it('should check for E-Prime compliance if explicitly activated', (done) => {
+    exec('./bin/write-good.js ./test/texts/English.md --eprime', (err, stdout, stderr) => {
+      expectedWarningsEprime.forEach((warning) => {
+        expect(stdout.includes(warning)).toBe(true);
+      });
+      done();
+    });
+  });
+
+  it('should support extensions', (done) => {
+    const expectedWarnings = [
       '"komplett" is a weasel word on line 1 at column 27',
       '"Aller Wahrscheinlichkeit nach" is wordy or unneeded on line 3 at column 0'
     ];
-    exec('./bin/write-good.js ./test/texts/German.md --checks=schreib-gut --weasel --tooWordy', function(err, stdout, stderr) {
-      expectedWarnings.forEach(function(warning) {
+    exec('./bin/write-good.js ./test/texts/German.md --checks=schreib-gut --weasel --tooWordy', (err, stdout, stderr) => {
+      expectedWarnings.forEach((warning) => {
         expect(stdout.includes(warning)).toBe(true);
       });
       done();
     });
   });
 
-  it('should show a meaningful error message if the user provides invalid command line arguments', function(done) {
-    exec('./bin/write-good.js ./test/texts/German.md --nonsense', function(err, stdout, stderr) {
+  it('should show a meaningful error message if the user provides invalid command line arguments', (done) => {
+    exec('./bin/write-good.js ./test/texts/German.md --nonsense', (err, stdout, stderr) => {
       expect(stdout.trim()).toEqual('"--nonsense" is not a valid argument.');
       done();
     });
   });
 
-  it('should show a meaningful error message if the import of the specified extension fails', function(done) {
-    exec('./bin/write-good.js ./test/texts/German.md --checks=nonsense', function(err, stdout, stderr) {
+  it('should show a meaningful error message if the import of the specified extension fails', (done) => {
+    exec('./bin/write-good.js ./test/texts/German.md --checks=nonsense', (err, stdout, stderr) => {
       expect(stdout.trim()).toEqual('Could not import custom check module. Check for spelling errors and make sure you have the module installed.');
       done();
     });
   });
 
-  it('should show a meaningful error message if the user provides invalid command line arguments and extension', function(done) {
-    exec('./bin/write-good.js ./test/texts/German.md --checks=schreib-gut --adverb --weasel', function(err, stdout, stderr) {
+  it('should show a meaningful error message if the user provides invalid command line arguments and extension', (done) => {
+    exec('./bin/write-good.js ./test/texts/German.md --checks=schreib-gut --adverb --weasel', (err, stdout, stderr) => {
       expect(stdout.trim()).toEqual('"--adverb" is not a valid argument.');
       done();
     });
   });
 
-  it('should show a meaningful error message if the user does not provide any file to check', function(done) {
-  exec('./bin/write-good.js', function(err, stdout, stderr) {
-    expect(stdout.trim()).toEqual('You did not provide any files to check.');
-    done();
-  });
+  it('should show a meaningful error message if the user does not provide any file to check', (done) => {
+    exec('./bin/write-good.js', (err, stdout, stderr) => {
+      expect(stdout.trim()).toEqual('You did not provide any files to check.');
+      done();
+    });
   });
 
-  it('should support globs', function(done) {
-    exec('./bin/write-good.js test/**/*.md', function(err, stdout, stderr) {
-      var english2Warnings = ['"Playing the field" is a cliche on line 1 at column 0'];
-      var expectedWarnings = expectedWarningsNotWeasel
+  it('should support globs', (done) => {
+    exec('./bin/write-good.js test/**/*.md', (err, stdout, stderr) => {
+      const english2Warnings = ['"Playing the field" is a cliche on line 1 at column 0'];
+      const expectedWarnings = expectedWarningsNotWeasel
         .concat(expectedWarningsOnlyWeasel)
         .concat(expectedWarningsPartWeaselPlus)
         .concat(english2Warnings);
-      expectedWarnings.forEach(function(warning) {
+      expectedWarnings.forEach((warning) => {
         expect(stdout.includes(warning)).toBe(true);
       });
       done();
     });
   });
 
-  it('should not try to read in directories as files when globs do not specify file ending', function(done) {
-    exec('./bin/write-good.js test/** --so', function(err, stdout, stderr) {
+  it('should not try to read in directories as files when globs do not specify file ending', (done) => {
+    exec('./bin/write-good.js test/** --so', (err, stdout, stderr) => {
       // this error is expected; we have a problem if we get a different error:
       expect(err.toString().trim()).toEqual('Error: Command failed: ./bin/write-good.js test/** --so');
       done();
--- write-good.js
+++ write-good.js
@@ -1,67 +1,65 @@
-var assign = Object.assign || require('object.assign');
+const assign = Object.assign || require('object.assign');
 
-var defaultChecks = {
-  weasel  : { fn: require('weasel-words'),            explanation: 'is a weasel word' },
-  illusion : { fn: require('./lib/lexical-illusions'), explanation: 'is repeated' },
-  so       : { fn: require('./lib/starts-with-so'),    explanation: 'adds no meaning' },
-  thereIs  : { fn: require('./lib/there-is'),          explanation: 'is unnecessary verbiage' },
-  passive  : { fn: require('passive-voice'),           explanation: 'may be passive voice' },
-  adverb   : { fn: require('adverb-where'),            explanation: 'can weaken meaning'},
-  tooWordy : { fn: require('too-wordy'),               explanation: 'is wordy or unneeded'},
-  cliches  : { fn: require('no-cliches'),              explanation: 'is a cliche'},
-  eprime   : { fn: require('e-prime'),                 explanation: 'is a form of \'to be\''}
+const defaultChecks = {
+  weasel: { fn: require('weasel-words'), explanation: 'is a weasel word' },
+  illusion: { fn: require('./lib/lexical-illusions'), explanation: 'is repeated' },
+  so: { fn: require('./lib/starts-with-so'), explanation: 'adds no meaning' },
+  thereIs: { fn: require('./lib/there-is'), explanation: 'is unnecessary verbiage' },
+  passive: { fn: require('passive-voice'), explanation: 'may be passive voice' },
+  adverb: { fn: require('adverb-where'), explanation: 'can weaken meaning' },
+  tooWordy: { fn: require('too-wordy'), explanation: 'is wordy or unneeded' },
+  cliches: { fn: require('no-cliches'), explanation: 'is a cliche' },
+  eprime: { fn: require('e-prime'), explanation: 'is a form of \'to be\'' }
 };
 
 // User must explicitly opt-in
-var disabledChecks = {
+const disabledChecks = {
   eprime: false
 };
 
 module.exports = function (text, opts) {
-  opts = opts ? opts : {};
-  var finalOpts = {};
+  opts = opts || {};
+  const finalOpts = {};
   opts = assign({}, disabledChecks, opts);
-  Object.keys(opts).map(function(optKey) {
-    if(optKey !== 'checks') {
+  Object.keys(opts).map((optKey) => {
+    if (optKey !== 'checks') {
       finalOpts[optKey] = opts[optKey];
     }
   });
 
-  var finalChecks = opts.checks || defaultChecks;
+  const finalChecks = opts.checks || defaultChecks;
 
-  var suggestions = [];
-  Object.keys(finalChecks).forEach(function (checkName) {
+  let suggestions = [];
+  Object.keys(finalChecks).forEach((checkName) => {
     if (finalOpts[checkName] !== false) {
-      suggestions = suggestions.concat(finalChecks[checkName].fn(text).
-                          map(reasonable(finalChecks[checkName].explanation)));
+      suggestions = suggestions.concat(finalChecks[checkName].fn(text)
+        .map(reasonable(finalChecks[checkName].explanation)));
     }
   });
 
-  return dedup(suggestions).sort(function (a, b) {
-    return a.index < b.index ? -1 : 1;
-  });
+  return dedup(suggestions).sort((a, b) => (a.index < b.index ? -1 : 1));
 
-  function reasonable (reason) {
+  function reasonable(reason) {
     return function (suggestion) {
-      suggestion.reason = '"' +
-          text.substr(suggestion.index, suggestion.offset) +
-          '" ' + reason;
+      suggestion.reason = `"${
+        text.substr(suggestion.index, suggestion.offset)
+      }" ${reason}`;
       return suggestion;
     };
   }
 };
 
 
-function dedup (suggestions) {
-  var dupsHash = {};
+function dedup(suggestions) {
+  const dupsHash = {};
 
-  return suggestions.reduce(function(memo, suggestion) {
-    var key = suggestion.index + ":" + suggestion.offset;
+  return suggestions.reduce((memo, suggestion) => {
+    const key = `${suggestion.index}:${suggestion.offset}`;
     if (!dupsHash[key]) {
       dupsHash[key] = suggestion;
       memo.push(suggestion);
     } else {
-      dupsHash[key].reason += " and " + suggestion.reason.substring(suggestion.offset + 3);
+      dupsHash[key].reason += ` and ${suggestion.reason.substring(suggestion.offset + 3)}`;
     }
     return memo;
   }, []);
